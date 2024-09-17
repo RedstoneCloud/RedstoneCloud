80f2127b0004c669f4a0565b1ef8adfa96ebc4ca
PleaseInsertNameHere <73995049+PleaseInsertNameHere@users.noreply.github.com>
2024-09-17 17:52:51 +0200

    add encryptedpacket to registry

--- api/src/main/java/de/redstonecloud/api/netty/NettyHelper.java
+++ api/src/main/java/de/redstonecloud/api/netty/NettyHelper.java
@@ -2,6 +2,7 @@ package de.redstonecloud.api.netty;
 
 import de.pierreschwang.nettypacket.registry.IPacketRegistry;
 import de.pierreschwang.nettypacket.registry.SimplePacketRegistry;
+import de.redstonecloud.api.netty.packet.EncryptedPacket;
 import de.redstonecloud.api.netty.packet.communication.ClientAuthPacket;
 import de.redstonecloud.api.netty.packet.player.PlayerConnectPacket;
 import de.redstonecloud.api.netty.packet.player.PlayerDisconnectPacket;
@@ -23,6 +24,7 @@ public class NettyHelper {
         Holder.registry = registry;
 
         try {
+            registry.registerPacket(EncryptedPacket.NETWORK_ID, EncryptedPacket.class);
             registry.registerPacket(ClientAuthPacket.NETWORK_ID, ClientAuthPacket.class);
 
             registry.registerPacket(GetBestTemplatePacket.NETWORK_ID, GetBestTemplatePacket.class);
